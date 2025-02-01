4c8bb1eba1c02e9331c9c93116941f1132849680
Hardik <malihardik88@gmail.com>
2025-02-01 11:57:50 +0530

    connecting neon database

--- app/query/route.ts
+++ app/query/route.ts
@@ -13,14 +13,44 @@
 // 	return data.rows;
 // }
 
-export async function GET() {
-  return Response.json({
-    message:
-      'Uncomment this file and remove this line. You can delete this file when you are finished.',
-  });
+// export async function GET() {
+//   return Response.json({
+//     message:
+//       'Uncomment this file and remove this line. You can delete this file when you are finished.',
+//   });
   // try {
   // 	return Response.json(await listInvoices());
   // } catch (error) {
   // 	return Response.json({ error }, { status: 500 });
   // }
+//}
+
+
+
+
+import { Pool } from "@neondatabase/serverless";
+
+const pool = new Pool();
+
+async function listInvoices() {
+    const client = await pool.connect();
+    try {
+        const data = await client.query(`
+            SELECT invoices.amount, customers.name
+            FROM invoices
+            JOIN customers ON invoices.customer_id = customers.id
+            WHERE invoices.amount = 666;
+        `);
+        return data.rows;
+    } finally {
+        client.release();
+    }
+}
+
+export async function GET() {
+    try {
+        return Response.json(await listInvoices());
+    } catch (error) {
+        return Response.json({ error }, { status: 500 });
+    }
 }
